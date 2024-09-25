efbeef01994a4d4c11c52ecb0d81b22bca21da4b
Yhya Ibrahim <yhyamobile57@gmail.com>
2024-09-25 16:04:32 +0300

    feat(nvim): add c support

--- stow/nvim/.config/nvim/lua/configs/lspconfig.lua
+++ stow/nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
-local servers = { "html", "cssls", "ts_ls", "pyright" }
+local servers = { "html", "cssls", "ts_ls", "pyright", "clangd" }
 
 local nvlsp = require "nvchad.configs.lspconfig"
 
