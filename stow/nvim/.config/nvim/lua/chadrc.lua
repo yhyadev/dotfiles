d2d4cec48e98c6d2bb5bd267524ac2ba351f79d8
Yhya Ibrahim <yhyamobile57@gmail.com>
2025-01-17 08:19:19 +0200

    chore: update tokyonight theme to use a little bit darker background

--- stow/nvim/.config/nvim/lua/chadrc.lua
+++ stow/nvim/.config/nvim/lua/chadrc.lua
@@ -14,6 +14,36 @@ M.base46 = {
     ["@punctuation.delimiter.zig"] = { fg = "light_grey" },
     ["@operator.zig"] = { fg = "cyan" },
   },
+
+  changed_themes = {
+    tokyonight = {
+      base_16 = {
+        base00 = "#11121d",
+        base01 = "#1b1c27",
+        base02 = "#21222d",
+        base03 = "#282934",
+        base04 = "#30313c",
+        base05 = "#abb2bf",
+        base06 = "#b2b9c6",
+      },
+
+      base_30 = {
+        white = "#A0A8CD",
+        darker_black = "#0c0d18",
+        black = "#11121D",
+        black2 = "#171823",
+        one_bg = "#1d1e29",
+        one_bg2 = "#252631",
+        one_bg3 = "#252631",
+        grey = "#474853",
+        grey_fg = "#474853",
+        grey_fg2 = "#4e4f5a",
+        light_grey = "#545560",
+        statusline_bg = "#161722",
+        lightbg = "#2a2b36",
+      },
+    },
+  },
 }
 
 M.ui = {
--- stow/nvim/.config/nvim/lua/plugins/init.lua
+++ stow/nvim/.config/nvim/lua/plugins/init.lua
@@ -17,6 +17,11 @@ return {
   },
 
   {
+    "NvChad/base46",
+    branch = "v3.0",
+  },
+
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
