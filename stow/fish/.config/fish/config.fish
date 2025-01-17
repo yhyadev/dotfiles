8f831c0ee9eedc19449572f3bacb7324cf6d7eba
Yhya Ibrahim <yhyamobile57@gmail.com>
2025-01-17 07:43:53 +0200

    feat(fish): add fish configuration

---
+++ stow/fish/.config/fish/config.fish
@@ -0,0 +1,29 @@
+set -U fish_greeting
+
+export LANG=en_US.UTF-8
+
+export PATH="$HOME/.local/bin:$PATH"
+
+# Zoxide
+zoxide init fish | source
+
+# Rust
+export PATH="$HOME/.cargo/bin:$PATH"
+
+# Go
+export PATH="$HOME/go/bin:$PATH"
+export GOROOT="/usr/local/lib/go"
+export GOPROXY=https://proxy.golang.org,direct
+export GOSUMDB=sum.golang.org
+export GOTOOLCHAIN=auto
+
+# C3
+export PATH="$HOME/.local/bin/c3/:$PATH"
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/home/yhya/.opam/opam-init/init.fish' && source '/home/yhya/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
