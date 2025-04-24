c70e0e6a7d9fd953aa7c3f0edfa32511069435c5
Yhya Ibrahim <yhyamobile57@gmail.com>
2025-04-24 05:56:38 +0200

    chore(fish): remove c3 and ghc from PATH

--- stow/fish/.config/fish/config.fish
+++ stow/fish/.config/fish/config.fish
@@ -17,9 +17,6 @@ export GOPROXY=https://proxy.golang.org,direct
 export GOSUMDB=sum.golang.org
 export GOTOOLCHAIN=auto
 
-# C3
-export PATH="$HOME/.local/bin/c3/:$PATH"
-
 # BEGIN opam configuration
 # This is useful if you're using opam as it adds:
 #   - the correct directories to the PATH
@@ -27,5 +24,3 @@ export PATH="$HOME/.local/bin/c3/:$PATH"
 # This section can be safely removed at any time if needed.
 test -r '/home/yhya/.opam/opam-init/init.fish' && source '/home/yhya/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
 # END opam configuration
-
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/yhya/.ghcup/bin # ghcup-env
